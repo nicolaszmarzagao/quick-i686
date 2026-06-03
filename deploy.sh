7a99ada2613ffda363f573281e1aaef95b5dfd8e
nicolaszmarzagao <nicolas.marzagao@gmail.com>
2026-06-03 17:05:20 -0300

    fix: broken 'makebin' option

--- deploy.sh
+++ deploy.sh
@@ -79,28 +79,28 @@ function pause {
 function SetVars {
 
     echo -e "\033[92mExport variables \033[0m"
-	export PREFIX="$HOME/.i686-elf/"
-	export TARGET=i686-elf
-	export PATH="$PREFIX/bin:$PATH"
+    export PREFIX="$HOME/.i686-elf/"
+    export TARGET=i686-elf
+    export PATH="$PREFIX/bin:$PATH"
 }
 
 function persistVars {
-	# echo "#compiler target arch variables for i686-elf-* (OSDev)"
-	echo 'export PREFIX="$HOME/.i686-elf/"' >> $HOME/.bashrc
-	echo "export TARGET=i686-elf" >> $HOME/.bashrc
-	echo 'export PATH="$PREFIX/bin:$PATH"' >> $HOME/.bashrc
+    # echo "#compiler target arch variables for i686-elf-* (OSDev)"
+    echo 'export PREFIX="$HOME/.i686-elf/"' >> $HOME/.bashrc
+    echo "export TARGET=i686-elf" >> $HOME/.bashrc
+    echo 'export PATH="$PREFIX/bin:$PATH"' >> $HOME/.bashrc
 }
 
 function mkdirs {
-	
+    
     echo -e "\033[92mCreating directories...\033[0m"
-	mkdir -p i686-elf-src
-	cd i686-elf-src 
-	# Make directories
-	mkdir -p build-binutils
-	mkdir -p build-gcc
-	mkdir -p build-gdb
-	mkdir -p $HOME/.i686-elf
+    mkdir -p i686-elf-src
+    cd i686-elf-src 
+    # Make directories
+    mkdir -p build-binutils
+    mkdir -p build-gcc
+    mkdir -p build-gdb
+    mkdir -p $HOME/.i686-elf
 }
 
 
@@ -125,51 +125,51 @@ function DownloadSources () {
     if [ $OUTPUT == false ]
     then
         wget -cq https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.$AT
-	    wget -cq https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.$AT
-    	wget -cq https://ftp.gnu.org/gnu/gdb/gdb-$GDB_VERSION.tar.$AT
+        wget -cq https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.$AT
+        wget -cq https://ftp.gnu.org/gnu/gdb/gdb-$GDB_VERSION.tar.$AT
 
     else
         wget -c https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.$AT
-	    wget -c https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.$AT
-	    wget -c https://ftp.gnu.org/gnu/gdb/gdb-$GDB_VERSION.tar.$AT
+        wget -c https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.$AT
+        wget -c https://ftp.gnu.org/gnu/gdb/gdb-$GDB_VERSION.tar.$AT
 
     fi
-	
-	if [ "$AT" == "gz" ]	
-	then
-		for filename in *.tar.gz
-		do	
-		    echo -e "\033[92mExtracting tar.gz archive...\033[0m"
+    
+    if [ "$AT" == "gz" ]    
+    then
+        for filename in *.tar.gz
+        do    
+            echo -e "\033[92mExtracting tar.gz archive...\033[0m"
             if [ $OUTPUT == false ]
             then
                 tar -xzf $filename > /dev/null
             else
-    			tar -xvzf $filename
+                tar -xvzf $filename
             fi
-		done
-	elif [ "$AT" == "xz" ]
-	then
-		for filename in *.tar.xz
-		do	
-		    echo -e "\033[92mExtracting tar.xz archive...\033[0m"
-			if [ $OUTPUT == false ]
+        done
+    elif [ "$AT" == "xz" ]
+    then
+        for filename in *.tar.xz
+        do    
+            echo -e "\033[92mExtracting tar.xz archive...\033[0m"
+            if [ $OUTPUT == false ]
             then
                 tar -xf $filename
             else
                 tar -xvf $filename
             fi
-		done
-	fi
+        done
+    fi
 
-	echo -e "\033[92mDownload GCC prerequisites\033[0m"
-	cd gcc-*/
+    echo -e "\033[92mDownload GCC prerequisites\033[0m"
+    cd gcc-*/
     if [ $OUTPUT == false ]
     then
         contrib/download_pre* > /dev/null
     else
         contrib/download_pre*
     fi
-	cd ..
+    cd ..
 }
 
 
@@ -185,18 +185,18 @@ function MakeBinutils {
     rm -rf build-binutils
     mkdir build-binutils
     cd build-binutils
-	
+    
     if [ $OUTPUT == false ]
     then
         ../binutils-$BINUTILS_VERSION/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror > binutils-configure.txt > /dev/null
         make > binutils-make.txt > /dev/null 
-	    make install > binutils-install.txt > /dev/null
+        make install > binutils-install.txt > /dev/null
     else
         ../binutils-$BINUTILS_VERSION/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror > binutils-configure.txt
         make > binutils-make.txt
-	    make install > binutils-install.txt
+        make install > binutils-install.txt
     fi
-	cd ..
+    cd ..
 }
 
 
@@ -273,7 +273,7 @@ function MakeGDB {
 function cleanUp {
 
     echo -e "\033[92mCleaning up source files...\033[0m"
-	rm -rf i686-elf-src
+    rm -rf i686-elf-src
 }
 
 
@@ -289,61 +289,61 @@ function main() {
     fi
 
     if [ "$arg" == "--clean" ] || [ "$arg" == "-c" ]
-	then
-		cleanUp
+    then
+        cleanUp
         exit
-	fi
+    fi
 
     SetVars
-	mkdirs
+    mkdirs
     
     if [ "$arg" == "--download" ] || [ "$arg" == "-dl" ]
-	then
-		DownloadSources
+    then
+        DownloadSources
         exit
-	elif [ "$arg" == "makebin" ]
-	then
-		echo -e "\033[92mMaking i686 Binutils\033[0m"
+    elif [ "$arg" == "makebin" ]
+    then
+        echo -e "\033[92mMaking i686 Binutils\033[0m"
         DownloadSources
-		MakeGDB
-		
-	elif [ "$arg" == "makegcc" ]
-	then
-		echo -e "\033[92mMaking i686 Binutils + GCC\033[0m"
-		DownloadSources
+	MakeBinutils 
+
+    elif [ "$arg" == "makegcc" ]
+    then
+        echo -e "\033[92mMaking i686 Binutils + GCC\033[0m"
+        DownloadSources
         MakeBinutils
-		MakeGCC
-		
-	elif [ "$arg" == "makegdb" ]
-	then
-		echo -e "\033[92mMaking i686 Binutils + GDB\033[0m"
+        MakeGCC
+        
+    elif [ "$arg" == "makegdb" ]
+    then
+        echo -e "\033[92mMaking i686 Binutils + GDB\033[0m"
         DownloadSources
-		MakeBinutils
-		MakeGDB
-		
-	elif [ "$arg" == "nopersist" ]
-	then
+        MakeBinutils
+        MakeGDB
+        
+    elif [ "$arg" == "nopersist" ]
+    then
         DownloadSources
         MakeBinutils
         MakeGCC
         MakeGDB
         
     else
-	    if [ $OUTPUT == false ]
+        if [ $OUTPUT == false ]
         then
             echo -e "\033[92mRunning quietly...\033[0m"
         else
             echo -e "\033[92mRunning normally...\033[0m"
-		fi
-		persistVars
-		DownloadSources
+        fi
+        persistVars
+        DownloadSources
 
-		MakeBinutils
-		MakeGCC
-		MakeGDB
+        MakeBinutils
+        MakeGCC
+        MakeGDB
 
-		
-	fi
+        
+    fi
     cleanUp
     popd
     exit
